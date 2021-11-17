f9050886df6610f61d07e0b08bdf63cff916bd2c
Maksudul Haque <saad.mk112@gmail.com>
2021-11-17 19:39:30 +0600

    Fix

--- scripts/image_upload_services.py
+++ scripts/image_upload_services.py
@@ -175,7 +175,7 @@ class GitHubBranchImageUploadService(ImageUploadServiceBase):
                     '[website-screenshots-action] '
                     f'Added Screenshots for PR #{self.pull_request_number}'
                 ),
-                'content': image_data,
+                'content': str(image_data),
                 'branch': self.new_branch,
                 'author': {
                     'name': self.username,
